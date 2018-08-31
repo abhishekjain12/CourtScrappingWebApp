1db9aa98fb46655f6519b6ed7c912cb16e436582
abhishek <jabhi1210@gmail.com>
2018-08-31 13:26:58 +0530

    Added Json uniques file functionality

--- Utils/db.py
+++ Utils/db.py
@@ -167,13 +167,11 @@ def select_count_query(table_name, case_no):
 def select_json_query(table_name, start_date, end_date):
     db = db_connect()
     try:
-        update_query("DELETE FROM Tracker_JSON WHERE Name = '" + table_name + "'")
-        insert_query("INSERT INTO Tracker_JSON (Name, Start_Date, End_Date, No_Files, emergency_exit, "
-                     "status) VALUES ('" + table_name + "', '" + start_date + "', '" + end_date + "', 0, false, "
-                                                                                                  "'IN_RUNNING')")
+        update_query("UPDATE Tracker_JSON SET Start_Date='" + start_date + "', End_Date='" + end_date +
+                     "', No_Files=0, emergency_exit=false, status='IN_RUNNING' WHERE Name='" + table_name + "'")
 
         cursor = db.cursor()
-        cursor.execute("select count(id) as num_rows from " + str(table_name))
+        cursor.execute("select count(id) as num_rows from " + str(table_name) + " WHERE is_json=0")
         result = cursor.fetchall()
         cursor.close()
         no_rows = result[0]['num_rows']
@@ -181,18 +179,25 @@ def select_json_query(table_name, start_date, end_date):
         no_of_data_per_iteration = 1000
         no_of_iteration = floor(int(no_rows) / no_of_data_per_iteration) + 1
 
+        j_count = select_one_query("SELECT json_count FROM Tracker_JSON WHERE Name='" + table_name + "'")['json_count']
+
         for i in range(0, no_of_iteration):
             cursor = db.cursor()
-            cursor.execute("SELECT * FROM " + str(table_name) + " LIMIT " + str(no_of_data_per_iteration) +
-                           " OFFSET " + str(i * no_of_data_per_iteration))
+            cursor.execute("SELECT * FROM " + str(table_name) + " WHERE is_json=0 LIMIT " +
+                           str(no_of_data_per_iteration) + " OFFSET " + str(i * no_of_data_per_iteration))
             result = cursor.fetchall()
             cursor.close()
 
-            file_path = module_directory + "/../Data_Files/JSON_Files/" + str(table_name) + "_" + str(i) + ".json"
+            file_path = module_directory + "/../Data_Files/JSON_Files/" + str(table_name) + "_" + \
+                        str(i + 1 + j_count) + ".json"
             fw = open(file_path, "w")
             fw.write(json.dumps(result))
 
-            update_query("UPDATE Tracker_JSON SET No_Files=No_Files+1 WHERE Name='" + table_name + "'")
+            for record in result:
+                update_query("UPDATE " + table_name + " SET is_json=1 WHERE id='" + str(record['id']) + "'")
+
+            update_query("UPDATE Tracker_JSON SET No_Files=No_Files+1, json_count=json_count+1 WHERE Name='" +
+                         table_name + "'")
 
         update_query("UPDATE Tracker_JSON SET status='IN_SUCCESS', emergency_exit=true WHERE Name='" +
                      table_name + "'")
