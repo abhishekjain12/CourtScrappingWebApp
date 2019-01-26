fac497d7bdbd2bd5f7778d0e837d5f300c29aa01
abhishek_thesunflowerlab <abhishek.jain@thesunflowerlab.com>
2019-01-26 14:19:53 +0530

    complete gst advance

--- new/utils/extract_text.py
+++ new/utils/extract_text.py
@@ -10,17 +10,20 @@ from io import StringIO
 
 def pdf_to_text_api(file_path):
     try:
-        text_data = ""
+        if file_path is not None:
+            text_data = ""
 
-        pdf_manager = PDFResourceManager()
-        string_io = StringIO()
-        pdf_to_text = TextConverter(pdf_manager, string_io, codec='utf-8', laparams=LAParams())
-        interpreter = PDFPageInterpreter(pdf_manager, pdf_to_text)
-        for page in PDFPage.get_pages(open(file_path, 'rb')):
-            interpreter.process_page(page)
-            text_data = string_io.getvalue()
+            pdf_manager = PDFResourceManager()
+            string_io = StringIO()
+            pdf_to_text = TextConverter(pdf_manager, string_io, codec='utf-8', laparams=LAParams())
+            interpreter = PDFPageInterpreter(pdf_manager, pdf_to_text)
+            for page in PDFPage.get_pages(open(file_path, 'rb')):
+                interpreter.process_page(page)
+                text_data = string_io.getvalue()
 
-        return str(text_data).strip().replace('\n', '').replace('\f', '')
+            return str(text_data).strip().replace('\n', '').replace('\f', '')
+        else:
+            return None
 
     except Exception as e:
         traceback.print_exc()
