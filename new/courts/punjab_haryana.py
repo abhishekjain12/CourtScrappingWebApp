e9cd282e618926b8ccba5cb070538348625731d3
abhishek <jabhi1210@gmail.com>
2019-03-02 17:33:42 +0530

    Removed the sleep and revert the clearInterval

--- new/courts/punjab_haryana.py
+++ new/courts/punjab_haryana.py
@@ -4,7 +4,6 @@ import os
 import traceback
 import logging
 
-from time import sleep
 from bs4 import BeautifulSoup
 from pymysql import escape_string
 from slugify import slugify
@@ -199,7 +198,6 @@ def request_data(court_name, headers):
 
                 if "no case found" in response.lower():
                     update_query("UPDATE tracker SET no_nodata=no_nodata+1 WHERE court_name=%s", (court_name))
-                    sleep(2)
                     break
                 else:
                     parser(response, court_name, headers)
--- static/javascript/new.js
+++ static/javascript/new.js
@@ -125,6 +125,7 @@ function current_new(court_name, bench) {
                 s_btn.removeClass("disabled");
                 $('#new-cancel-btn').addClass("d-none");
                 s_btn.removeClass("d-none");
+                clearInterval(new_interval);
             }
             else {
                 tr_list += "<tr>";
@@ -148,6 +149,7 @@ function current_new(court_name, bench) {
                 if ("IN_RUNNING" !== data.status){
                     $('#new-cancel-btn').addClass("d-none");
                     $('#new-submit-btn').removeClass("d-none");
+                    clearInterval(new_interval);
                 }
             }
             $("#new-current").html(tr_list);
@@ -182,6 +184,7 @@ function running_new() {
                  s_btn.removeClass("disabled");
                 $('#new-cancel-btn').addClass("d-none");
                 s_btn.removeClass("d-none");
+                clearInterval(new_interval);
             }
             else {
                 tr_list += "<tr>";
@@ -215,6 +218,7 @@ function running_new() {
                 else {
                     $('#new-cancel-btn').addClass("d-none");
                     $('#new-submit-btn').removeClass("d-none");
+                    clearInterval(new_interval);
                 }
             }
             $("#new-current").html(tr_list);
